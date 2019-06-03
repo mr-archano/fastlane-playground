92c1f547078c51a96108874377b26bda8b239f27
Antonio Bertucci <archano@gmail.com>
2019-06-03 17:39:57 +0200

    Init project

---
+++ Fastlane Playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fastlane Playground
+//
+//  Created by Antonio on 03.06.19.
+//  Copyright © 2019 Antonio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
